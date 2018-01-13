e55fdd891b5bb590ce2d40cb76bb8180f20489fd
Martoon <martoon.hsk@gmail.com>
2018-01-13 18:38:34 +0300

    OneMax better solution

--- onemax/Main.hs
+++ onemax/Main.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns        #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 -- | ~3*n/4 solution of OneMax problem.
 
 module Main where
 
-import           Control.Monad         (forM, forM_, void)
+import           Control.Monad         (forM, forM_, replicateM, void)
+import           Data.List             (group, sort)
 import           Prelude               hiding (interact)
 import           System.IO             (hClose, hFlush, stdin, stdout)
 
@@ -12,16 +14,15 @@ import           Control.Monad.ST.Lazy as ST
 import qualified Data.Array            as A
 import qualified Data.Array.IO.Safe    as A
 import qualified Data.Array.ST.Safe    as A
+import qualified Data.Bits             as B
 
 import           Debug.Trace
 
 main :: IO ()
 main = interact $ map (unwords . map show) . process . map read . words
   where
-    process (n:fs) = map (confuse' n) $ solve n fs
+    process (n:fs) = solve n fs
     process _      = error "not enought input"
-
-    confuse' = confuse 234235652
 
 interact :: (String -> [String]) -> IO ()
 interact f = do
@@ -29,13 +30,24 @@ interact f = do
     forM_ (f input) $ \line -> putStrLn line >> hFlush stdout
     mapM_ hClose [stdout, stdin]
 
+type Seed = Int
 type Fitness = Int
 type Flips = [Int]
 type InputOutput = [Fitness] -> [Flips]
 
-genPermutation :: Int -> Int -> A.Array Int Int
-genPermutation seed n = A.runSTArray $ do
-    array <- A.newListArray (1, n) [1..n]
+random :: Seed -> (Seed, Int)
+random s = (s * 99139) `divMod` 95279
+
+randomListR :: Enum a => Seed -> (a, a) -> [a]
+randomListR s (l, r) =
+    let (s', g) = random s
+        i = (g `mod` (fromEnum r - fromEnum l + 1)) + fromEnum l
+    in  toEnum i : randomListR s' (l, r)
+
+shuffle :: Seed -> [a] -> A.Array Int a
+shuffle seed content = A.runSTArray $ do
+    let n = length content
+    array <- A.newListArray (1, n) content
     go n (seed * 237) array
     return array
   where
@@ -49,6 +61,9 @@ genPermutation seed n = A.runSTArray $ do
         A.writeArray a k y
         go (k - 1) s' a
 
+genPermutation :: Seed -> Int -> A.Array Int Int
+genPermutation seed n = shuffle seed [1..n]
+
 confuse :: Int -> Int -> Flips -> Flips
 confuse seed n =
     let permutation = genPermutation seed n
@@ -60,7 +75,7 @@ addToNext f' []     = [f']
 
 -- | Helper for safe requests
 request :: Flips -> (Fitness -> InputOutput) -> InputOutput
-request req f inpss = req : let (!inp:inps) = inpss in f inp inps
+request req f inpss = cleanFlips req : let (!inp:inps) = inpss in f inp inps
 
 atLeastOneFlip :: [Flips] -> [Flips]
 atLeastOneFlip [] = [[1], [1]]
@@ -79,35 +94,58 @@ expect x y
 limit :: Int -> Int
 limit n = (+ 50) . round $ (fromIntegral n / 4 * 3 :: Double)
 
+cleanFlips :: Flips -> Flips
+cleanFlips = map head . filter (odd . length) . group . sort
+
+chunkSize :: Int
+chunkSize = 10
+
 solve :: Int -> InputOutput
-solve n inps = let (fi:fs) = inps in noMoreThan (limit n) . atLeastOneFlip $ go fi n fs
+solve n inps =
+    let (fi:fs) = inps
+    in noMoreThan (limit n) . atLeastOneFlip . map cleanFlips $ process fi 1 fs
   where
-    go fl 0 =
-        -- found solution
-        expect fl n
-    go fl 1
-        -- check whether last element should be flipped
-        | fl == n = const []
-        | otherwise = request [1] $ expect n
-    go !fl k =
-        -- try to flip next 2 and see what happens
-        let b1 = k
-            b2 = k - 1
-        in  request [b1, b2] $ \f ->
-            case f - fl of
-                -- entire profit - go further
-                2  -> go f (k - 2)
-                -- entire loss - flip those back on next request
-                -2 -> addToNext [b1, b2] . go fl (k - 2)
-                -- check which of the two gives profit
-                0  -> request [b2] $ \f2 ->
-                    case f2 - fl of
-                        -- optimized - go further
-                        1  -> go f2 (k - 2)
-                        -- made worse, fix on next iteration
-                        -1 -> addToNext [b1, b2] . go (fl + 1) (k - 2)
-                        o  -> error ("got fitness diff " ++ show o ++ "??")
-                o  -> error ("got fitness diff " ++ show o ++ "?")
+    process !f i
+        | i > n = expect f n
+        | otherwise =
+        let len = min chunkSize (n - i + 1)
+            initVariants = buildVariantsCached len
+            indices = [i .. len + i - 1]
+
+            go seed variants =
+                let reqMask = randomListR seed (False, True)
+                    req = applyMask reqMask indices
+                in  request req $ \f' ->
+                    let diff = f' - f
+                        variants' = filter (variantFits reqMask diff) variants
+                    in  (addToNext req .) $
+                            unless (f' == n) $
+                            case variants' of
+                                [] -> error "No variants remained!"
+                                [v] ->
+                                    let req' = map fst . filter ((== 0) . snd) $ zip indices v
+                                    in  -- trace ("Final in chunk, " ++ show v) $
+                                        request req' $ \f'' ->
+                                                         unless (f'' == n) $
+                                                 process f'' (i + len)
+                                vars -> go (snd $ random seed) vars
+        in go 234234 initVariants
+
+    variantFits mask diff variant =
+        let view = applyMask mask variant
+        in  diff == length view - 2 * (sum view)
+
+    applyMask :: [Bool] -> [a] -> [a]
+    applyMask m l = map snd . filter fst $ zip m l
+
+    unless :: Bool -> InputOutput -> InputOutput
+    unless False io = io
+    unless True _   = const []
+
+    buildVariants len = A.elems $ shuffle 2345 $ replicateM len [0, 1]
+    buildVariantsDef = buildVariants chunkSize
+    buildVariantsCached len = if len == 10 then buildVariantsDef else buildVariants len
+
 
 test :: (Int -> InputOutput) -> [Int] -> IO ()
 test solution initial = doTest initial $ solution (length initial)
