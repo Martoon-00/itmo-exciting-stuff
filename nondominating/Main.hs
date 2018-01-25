f83bb0a708001b5eac8cf023e8d013a99e5d5508
Martoon <martoon.hsk@gmail.com>
2018-01-25 15:37:01 +0300

    Add range tree

--- nondominating/Main.hs
+++ nondominating/Main.hs
@@ -9,14 +9,18 @@
 module Main where
 
 import           Control.Arrow            ((&&&))
-import           Control.Monad            (unless, when)
+import           Control.Monad            (forM, forM_, unless, when)
 import           Control.Monad.Fix        (fix)
+import qualified Control.Monad.ST         as ST
 import           Control.Monad.State      (State, evalState, get, modify, put, runState)
 import           Data.Bifunctor           (first, second)
 import           Data.Function            (on)
 import           Data.Maybe               (fromJust, fromMaybe)
+import           Data.Monoid              (Sum (..))
 import           Data.Monoid              ((<>))
 import           Data.Ord                 (comparing)
+import qualified Data.Set                 as S
+import qualified Data.STRef.Strict        as ST
 import           GHC.Exts                 (build)
 import           Prelude
 
@@ -43,6 +47,8 @@ type Point = V.Vector Coord
 type Rank = Word
 
 -- * Utils
+
+-- ** Median
 
 splitOnMed :: Ord b => (a -> b) -> V.Vector a -> (V.Vector a, V.Vector a, V.Vector a)
 splitOnMed cmp v =
@@ -84,6 +90,92 @@ chunksOf i ls = map (take i) (build (splitter ls)) where
     splitter :: [e] -> ([e] -> a -> a) -> a -> a
     splitter ![] _ n = n
     splitter l c n   = l `c` splitter (drop i l) c n
+
+-- ** Segment tree
+
+data SegmentTreeStructure s k v m
+    = Node (SegmentTreeStructure s k v m) (k -> Bool) (SegmentTreeStructure s k v m) (ST.STRef s m)
+    | Leaf k (ST.STRef s (S.Set v))
+
+data SegmentTree s k v m = SegmentTree
+    { stStructure  :: SegmentTreeStructure s k v m
+    , stGetKey     :: v -> k
+    , stGetSummary :: S.Set v -> m
+    }
+
+mkSegTree
+    :: forall s k v m.
+       (Ord k, Monoid m)
+    => (v -> k) -> (S.Set v -> m) -> [v] -> ST.ST s (SegmentTree s k v m)
+mkSegTree stGetKey stGetSummary allValues = do
+    stStructure <- go uniqKeys
+    return SegmentTree{..}
+  where
+    uniqKeys = V.fromList . nubSeqBy id . L.sort $ map stGetKey allValues
+    go keys
+        | V.length keys == 0 = error "Can't make segment tree of no values"
+        | V.length keys == 1 = Leaf (V.head keys) <$> ST.newSTRef S.empty
+        | otherwise = do
+              let med = V.length keys `div` 2
+              mr <- ST.newSTRef mempty
+              l <- go $ V.take med keys
+              r <- go $ V.drop med keys
+              let d = (< keys V.! med)
+              return $ Node l d r mr
+
+getSegElemSummary :: Monoid m => (S.Set v -> m) -> SegmentTreeStructure s k v m -> ST.ST s m
+getSegElemSummary stGetSummary = \case
+    Leaf _ setv -> stGetSummary <$> ST.readSTRef setv
+    Node _ _ _ mr -> ST.readSTRef mr
+
+updateSegTree :: Monoid m => k -> SegmentTree s k v m -> ST.ST s ()
+updateSegTree k SegmentTree{..} = go stStructure
+  where
+    go = \case
+        Leaf _ _ -> pure ()
+        Node l d r mr -> do
+            if d k then go l else go r
+            mapM (getSegElemSummary stGetSummary) [l, r]
+                >>= ST.writeSTRef mr . mconcat
+
+modifySegTree
+    :: Monoid m
+    => k -> (S.Set v -> S.Set v) -> SegmentTree s k v m -> ST.ST s ()
+modifySegTree k f st@SegmentTree{..} = do
+    go stStructure
+    updateSegTree k st
+  where
+    go = \case
+        Leaf _ setv -> ST.modifySTRef setv f
+        Node l d r _ ->
+            if d k then go l else go r
+
+insertSegTree :: (Monoid m, Ord v) => v -> SegmentTree s k v m -> ST.ST s ()
+insertSegTree v st@SegmentTree{..} = modifySegTree (stGetKey v) (S.insert v) st
+
+deleteSegTree :: (Monoid m, Ord v) => v -> SegmentTree s k v m -> ST.ST s ()
+deleteSegTree v st@SegmentTree{..} = modifySegTree (stGetKey v) (S.delete v) st
+
+-- | Request or range, inclusively.
+requestSegTree :: (Monoid m, Bounded k, Ord k) => (k, k) -> SegmentTree s k v m -> ST.ST s m
+requestSegTree range SegmentTree{..} = go range stStructure
+  where
+    go (keyL, keyR) = \case
+        l@(Leaf k _) ->
+            if keyL <= k && k <= keyR
+            then getSegElemSummary stGetSummary l
+            else pure mempty
+        n@(Node l isLeft r _) ->
+            if (keyL, keyR) == (minBound, maxBound)
+                then getSegElemSummary stGetSummary n
+            else if isLeft keyR
+                then go (keyL, keyR) l
+            else if not (isLeft keyL)
+                then go (keyL, keyR) r
+            else
+                (<>) <$> go (keyL, maxBound) l <*> go (minBound, keyR) r
+
+-- ** Utils
 
 dominates :: Point -> Point -> Bool
 dominates (V.toList -> p1) (V.toList -> p2) = and
@@ -245,19 +337,20 @@ sortSweepLine = \case
         let maxRank = getMaxFrontRank front
         let newMeta = updateRankIfNotKnown maxRank meta
         insert getLineKey newMeta
-        -- !_ <- traceM ("Inserting " ++ show newMeta)
+        !_ <- traceM ("Inserting " ++ show newMeta)
         return newMeta
     lowerPoint :: (PointMeta -> (Coord, Rank)) -> PointMeta -> PointMeta -> LineState PointMeta
     lowerPoint getLineKey oldMeta meta = do
         let key = getLineKey meta
-        (line, _) <- get
+        (line, front) <- get
         let belowRank = metaRank . snd <$> M.lookupLE key line
         let newRank = maybe 0 (+1) belowRank
+        -- update line
         when (newRank == metaRank oldMeta) $
-            delete getLineKey oldMeta
+            delete getLineKey $ (\(Just x) -> x) $ M.lookup newRank front
         let newMeta = updateRankIfNotKnown newRank meta
         insert getLineKey newMeta
-        -- !_ <- traceM ("Replacing " ++ show oldMeta ++ " with " ++ show newMeta)
+        !_ <- traceM ("Replacing " ++ show oldMeta ++ " with " ++ show newMeta)
         return newMeta
 
 sortSAC
@@ -273,16 +366,16 @@ sortSAC d points   -- TODO: extended base
         let med = findMed id $ fmap (getCoord . fst) points  -- TODO: try no decorate?
             comparingToMed (p, _) = getCoord p `compare` med
             (pointsL, pointsM, pointsR) = split comparingToMed points
-            -- !_ = trace ("loop split " ++ show (pointsL, pointsM, pointsR)) ()
+            !_ = trace ("loop split " ++ show (pointsL, pointsM, pointsR)) ()
 
             sortedL = sortSAC d pointsL
-            -- !_ = trace ("loop sortedL: " ++ show sortedL) ()
+            !_ = trace ("loop sortedL: " ++ show sortedL) ()
             sortedM = sortSAC (d - 1) $
                       updateSAC d sortedL pointsM
-            -- !_ = trace ("loop sortedM: " ++ show sortedM) ()
+            !_ = trace ("loop sortedM: " ++ show sortedM) ()
             sortedR = sortSAC d $
                       updateSAC d (sortedL <> sortedM) pointsR
-            -- !_ = trace ("loop sortedR: " ++ show sortedR) ()
+            !_ = trace ("loop sortedR: " ++ show sortedR) ()
         in  mconcat [sortedL, sortedM, sortedR]
   where
     getCoord = (V.! (d - 1))
@@ -310,16 +403,16 @@ updateSAC d known req   -- TODO: extended base
             (knownL, knownM, knownR) = split comparingToMed known
             (reqL, reqM, reqR) = split comparingToMed req
 
-            -- !_ = trace ("Updating over " ++ show known) ()
+            !_ = trace ("Updating over " ++ show known) ()
 
             sortedL = updateSAC d knownL reqL
-            -- !_ = trace ("update sortedL: " ++ show sortedL) ()
+            !_ = trace ("update sortedL: " ++ show sortedL) ()
             sortedM = updateSAC (d - 1) knownM $
                       updateSAC (d - 1) knownL reqM
-            -- !_ = trace ("update sortedM: " ++ show sortedM) ()
+            !_ = trace ("update sortedM: " ++ show sortedM) ()
             sortedR = updateSAC d knownR $
                       updateSAC d (knownL <> knownM) reqR
-            -- !_ = trace ("update sortedR: " ++ show sortedR) ()
+            !_ = trace ("update sortedR: " ++ show sortedR) ()
         in  mconcat [sortedL, sortedM, sortedR]
   where
     getCoord = (V.! (d - 1))
@@ -336,6 +429,34 @@ solve solver d points =
         | (point, rank) <- answer
         , id' <- getOrder point
         ]
+
+testSegTree :: Maybe Int -> IO ()
+testSegTree seed = Q.quickCheckWith args $
+    Q.forAll (Q.resize 20 $ Q.listOf1 $ (,) <$> Q.choose (-5, 5) <*> Q.choose (0, 10)) $
+        \((nubSeqBy id . L.sort -> input) :: [(Int, Word)]) ->
+    Q.forAll (Q.sublistOf input `Q.suchThat` (not . null)) $
+        \(toAdd :: [(Int, Word)]) ->
+    Q.forAll (fmap pure $ Q.resize 7 Q.arbitrary `Q.suchThat` \(l, r) -> l <= r) $
+        \(toRequest :: [(Int, Int)]) ->
+    let segAns = runWithSegTree input toAdd toRequest
+        dumbAns = runDumb toAdd toRequest
+    in  if segAns == dumbAns
+        then Q.succeeded
+        else Q.failed
+             { Q.reason = "Expected " ++ show dumbAns ++ ", got " ++ show segAns }
+  where
+    args =
+        Q.stdArgs
+        { Q.replay = fmap (\s -> (Q.mkQCGen s, s)) seed
+        , Q.maxSuccess = 10000
+        }
+    runDumb :: [(Int, Word)] -> [(Int, Int)] -> [Word]
+    runDumb present =
+        map $ \(reqL, reqR) -> sum . map snd $ filter (\(x, _) -> reqL <= x && x <= reqR) present
+    runWithSegTree input toAdd toRequest = ST.runST $ do
+        tree <- mkSegTree fst (foldMap (Sum . snd)) input
+        forM_ toAdd $ \added -> insertSegTree added tree
+        forM toRequest $ \req -> getSum <$> requestSegTree req tree
 
 genInput :: Int -> Int -> Q.Gen [Point]
 genInput d pointsNum =
